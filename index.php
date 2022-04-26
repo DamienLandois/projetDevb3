d7995bd80512add3d570d1feea1eebb839c7b63c
Squirrhell <32261427+Squirrhell@users.noreply.github.com>
2022-04-26 10:57:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <title>Oui</title>
+    <title>Pathfinder</title>
 </head>
 <body>
     <canvas id="leCanvas" width="600" height="600">
